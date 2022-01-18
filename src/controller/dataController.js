c3d6df4703ac7e6f8ee1533d80a8affe287af1f8
Chainruledydx <60750530+Chainruledydx@users.noreply.github.com>
2022-01-18 10:40:06 +0900

    blueprint of standard view

--- src/controller/dataController.js
+++ src/controller/dataController.js
@@ -1,34 +1,31 @@
-const dataFrame = [
-    {
-        name : "서방",
-        code : "0055WE",
-        category : ["낙농제품 및 식용 빙과류 제조업", "곡물 가공품 제조업"],
-        sales : 17000000
-    },
-    {
-        name : "동방",
-        code : "0072EA",
-        category : ["자동차 차체 및 트레일러 제조업", "자동차용 기타 신품 부품 제조업", "자동차용 엔진 제조업"],
-        sales : 27800000
-    },
-    {
-        name : "남방",
-        code : "0012SO",
-        category : ["섬유, 직물 및 의복 액세서리 소매업", "의복 소매업", "가전제품 소매업"],
-        sales : 36000000
-    },
-    {
-        name : "북방",
-        code : "0062NO",
-        category : ["컴퓨터 및 주변장치, 소프트웨어 및 통신기기 소매업", " 전기용품 및 조명장치 소매업"],
-        sales : 29500000
-    },
-]
+import {dataFrame} from "../model/fakeDB"
 
 export const getData = (req, res) => {
     res.render("data", {dataFrame})
 }
 
 export const getDataDetails = (req, res) => {
-    res.render("dataDetail")
+    const {id} = req.params
+    const detailed = dataFrame.find(src => src.code === id)
+    if(!detailed){
+        res.redirect("/data")
+    }
+    res.render("dataDetail", {detailed})
+}
+
+export const getDataEdit = (req, res) => {
+    const {id} = req.params
+    const detailed = dataFrame.find(src => src.code === id)
+    res.render("edit", {detailed})
+}
+
+export const getDataDelete = (req, res) => {
+    const {id} = req.params
+    if (!id){
+        res.redirect("/data")
+    }
+    // message는 세션으로 처리할 것.
+    const message = `someone make attempt to delete -> ${id}`
+    console.log(message)
+    res.status(401).redirect("/")
 }
--- src/router/dataRouter.js
+++ src/router/dataRouter.js
@@ -1,9 +1,11 @@
 import express from "express"
-import {getData, getDataDetails} from "../controller/dataController";
+import {getData, getDataDetails, getDataDelete, getDataEdit} from "../controller/dataController";
 
 const dataRouter = express.Router()
 
 dataRouter.get("/", getData)
 dataRouter.get("/:id", getDataDetails)
+dataRouter.get("/:id/edit", getDataEdit)
+dataRouter.get("/:id/delete", getDataDelete)
 
 export default dataRouter
